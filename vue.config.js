6176c699db47c5fe22640938b41b9fdfdc4ef360
YouCodify <luisdaviduribe@hotmail.com>
2025-02-20 01:49:27 -0400

    Cambios

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: './',
+  publicPath: '/mi-gomera/dist/',
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
